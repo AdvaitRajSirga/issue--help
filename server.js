b484e1974ea6149fcb841ebf1035835e0d959daf
ADVAIT RAJ SIRGA <91474902+AdvaitRajSirga@users.noreply.github.com>
2022-02-01 10:31:41 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require ("multer");
+const path = require('path');
+const app = express();
+const nodemailer = require('nodemailer');
+const { stringify } = require("querystring");
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var upload = multer({dest: './public/uploads/'});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+let mailTransporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+      user: 'demonyatoclan@gmail.com',
+      pass: 'advaitojaslohith'
+  }
+});
+
+
+app.post('/send', upload.single('myFile'), (req, res) => {
+var subName = req.body.Name
+var print = req.body
+console.log(print)
+
+
+
+let mailDetails = {
+  from: 'demonyatoclan@gmail.com',
+  to: 'demonyatoclan@gmail.com',
+  subject: 'Feedback',
+    text: "Details:" + " " + JSON.stringify(print),
+    attachments: [{'filename': req.file.originalname,
+    path: req.file.path
+}]
+}
+    
+  
+
+mailTransporter.sendMail(mailDetails, function(err, data) {
+  if(err) {
+      console.log('Error Occurs');
+  } else {
+      console.log('Email sent successfully');
+  }
+});
+
+res.send("Hello " + subName + ", Thank you for feedback")
+});
